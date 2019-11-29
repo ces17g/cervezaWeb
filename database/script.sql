e53f32dee972be18a7a7d1c4cfac227956215894
ces17g <54723379+ces17g@users.noreply.github.com>
2019-11-29 14:49:48 -0500

    Must create tables before referencing

--- database/script.sql
+++ database/script.sql
@@ -1,3 +1,18 @@
+
+CREATE TABLE City(
+    Name CHAR(30) PRIMARY KEY,
+    Distance REAL,
+    County CHAR(30));
+
+CREATE TABLE Bar(
+    Name CHAR(60) PRIMARY KEY,
+    Price_Range CHAR(5),
+    Address CHAR(128),
+    Type CHAR(30),
+    Year_Est INT,
+    City CHAR(30),
+    FOREIGN KEY (City) REFERENCES City(Name));
+
 CREATE TABLE User(
     UserID CHAR(30) PRIMARY KEY,
     Name CHAR(255) NOT NULL,
@@ -10,17 +25,3 @@ CREATE TABLE Rating(
     Rate INT,
     Bar CHAR(60),
     FOREIGN KEY (Bar) REFERENCES Bar(Name));
-
-CREATE TABLE Bar(
-    Name CHAR(60) PRIMARY KEY,
-    Price_Range CHAR(5),
-    Address CHAR(128),
-    Type CHAR(30),
-    Year_Est INT,
-    City CHAR(30),
-    FOREIGN KEY (City) REFERENCES City(Name));
-
-CREATE TABLE City(
-    Name CHAR(30) PRIMARY KEY,
-    Distance REAL,
-    County CHAR(30));
